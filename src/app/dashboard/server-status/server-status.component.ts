aa8b3c512ea4f5b6c7a37ea73fddcb4f164ac2e5
Flavia Kosti <flaviakosti@gmail.com>
2024-07-01 16:40:22 -0400

    Dashboard: Dynamic classes

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -9,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class ServerStatusComponent {
   currentStatus = 'offline';
+
+  constructor() {
+    const interval = setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.99999999999999
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
